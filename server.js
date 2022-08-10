3714bb75ef69298a12a69b4edc91ae0851a94a30
전준영 <wnsdud9476@gmail.com>
2022-08-10 23:37:11 +0900

    [ADD] 게시물 조회하기 수정 완료

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const express = require("express");
+const { createUser } = require("./app");
+const { createPost } = require("./app");
+const { viewingListofPosts } = require("./app");
+// const { modifyPost } = require("./app");
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", createUser);
+app.post("/post", createPost);
+app.get("/posts-list", viewingListofPosts);
+// app.patch("/modify-post", modifyPost);
+const server = http.createServer(app);
+
+server.listen(4000, () => {
+  console.log("server is listening on PORT 4000");
+});
