826d59bacfc9ba619d004bde1cb2f716af0f9b82
JUN <wnsdud9476@gmail.com>
2022-08-21 00:14:00 +0900

    중간수정

--- server.js
+++ server.js
@@ -17,9 +17,15 @@ const myDataSource = new DataSource({
   database: process.env.TYPEORM_DATABASE,
 });
 
-myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
-});
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Database initiate fail");
+  });
 
 app.get("/ping", (req, res, next) => {
   res.json({ message: "pong" });
@@ -75,9 +81,10 @@ app.post("/signin", (req, res) => {
     });
 });
 
-app.post("/post", (req, res) => {
+app.post("/post", async (req, res) => {
   const { user_id, contents } = req.body;
-  myDataSource.query(
+
+  await myDataSource.query(
     `INSERT INTO postings(
        user_id,
        contents
@@ -85,13 +92,12 @@ app.post("/post", (req, res) => {
     `,
     [user_id, contents]
   );
-  res.status(200).json({ message: "postCreated" });
+  res.status(201).json({ message: "postCreated" });
 });
 
-app.get("/posts-list", (req, res) => {
-  myDataSource
-    .query(
-      `select 
+app.get("/posts-list", async (req, res) => {
+  const postingList = await myDataSource.query(
+    `select 
         users.id as userId, 
         users.profile_image as userProfileImage, 
         postings.id as postingId, 
@@ -100,43 +106,68 @@ app.get("/posts-list", (req, res) => {
         FROM users 
         INNER JOIN postings ON postings.user_id = users.id 
         INNER JOIN posting_images ON posting_images.posting_id = postings.id;`
-    )
-    .then((value) => {
-      res.status(200).json({ data: value });
-    })
-    .catch((err) => res.status(500).json({ message: "server error" }));
+  );
+
+  res.status(200).json({ data: postingList });
 });
 
-app.patch("/modify-post", (req, res) => {
+app.get("/posts_list2", async (req, res) => {
+  const { id } = req.body;
+  const postingList = await myDataSource.query(
+    `
+      SELECT 
+        user_id as userId,
+        users.profile_image as userProfileImage, 
+        JSON_ARRAYAGG( 
+          JSON_OBJECT(
+            'postingId',postings.id, 
+            'postingImageUrl',posting_images.image_url, 
+            'postingContent',postings.contents
+          )
+        ) as postings 
+      FROM postings 
+      JOIN users ON postings.user_id = users.id 
+      JOIN posting_images ON posting_images.posting_id = postings.id 
+      WHERE users.id = ? 
+      GROUP BY users.id;
+  `,
+    [id]
+  );
+  console.log(postingList);
+  res.status(200).json({ data: postingList });
+});
+
+app.patch("/modify-post", async (req, res) => {
   const { id, user_id, contents } = req.body;
 
-  myDataSource
-    .query(
-      ` UPDATE postings SET contents = ? WHERE id = ? AND user_id = ?
+  await myDataSource.query(
+    ` UPDATE postings 
+          SET contents = ? 
+          WHERE id = ? 
   `,
-      [contents, user_id, id]
-    )
-    .then(() => {
-      myDataSource
-        .query(
-          `SELECT 
-            users.id as userId, users.nickname as userName, 
-            postings.id as postingId, postings.contents as postingContents 
-            FROM users
-            INNER JOIN postings ON postings.user_id = users.id AND postings.id = ? WHERE users.id = ?`,
-          [id, user_id]
-        )
-        .then((value) => {
-          res.status(200).json({ value });
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: error });
-    });
+    [contents, user_id, id]
+  );
+  const modifyData = await myDataSource.query(
+    `SELECT 
+            postings.user_id as userId, 
+            users.nickname as userName, 
+            postings.id as postingId, 
+            postings.contents as postingContents 
+            FROM postings
+            JOIN users ON postings.user_id = users.id 
+            WHERE postings.id = ? 
+            `,
+    [id]
+  );
+  res.status(200).json({ data: modifyData });
 });
 
-// app.delete("/post-delete", (req,res) => {});
+app.delete("/post-delete", async (req, res) => {
+  const { id } = req.query;
+
+  await myDataSource.query(`DELETE FROM postings WHERE postings.id =?`, [id]);
+  res.status(204).json({ message: "deleted" });
+});
 
 app.listen(4000, () => {
   console.log("server is listening on PORT 4000");
